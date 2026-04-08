887164b1781d36cd372fd7a6f69992da9ccfce8f
altamisatmaja <altamisfattah16@gmail.com>
2026-04-08 15:50:32 +0700

    fix: delete obs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+    build: {
+        minify: false,
+        sourcemap: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
